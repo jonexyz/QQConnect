f2d1421d6f6c9b1fdecca40176018d814aa0b5ad
Jone <abc@jone.xyz>
2022-08-07 10:40:04 +0800

    no message

--- README.md
+++ README.md
@@ -1,9 +1,91 @@
 # QQConnect
-QQ互联，基于官方sdk封装的composer包
+只是为了方便自己写网站使用，官方的sdk包用起来很是不方便，每次使用都要翻看文档和示例。
 
-为了方便自己写网站使用，官方的sdk包用起来很是不方便，每次使用都要翻看文档和示例
+QQ互联，基于官方sdk包修改与封装，另许多框架都对session操作进行了重新封装，避免在框架中使用时无法读取$_SESSION数据，这里将不做任何的数据存储，access_token 的有效期维护，需要自行处理。
 
-so，还是自己封装一下吧，毕官方的SDK包也是很久没更新了，所以这个SDK维护起来应该也是很轻松的。
 
-### 食用说明
+### 说明
+官方文档 https://wiki.connect.qq.com/
+1. access_token 过期时间为30天。如果存储的access token过期，请重新走登录流程，access token 的数据缓存需自行维护。
+2. 代码中使用了 $_GET 全局变量获取数据。
+3. 代码不适用于 swoole 类型的常驻内存框架使用。
+4. 如需避免抛出的异常信息暴露给用户，建议使用 try{ }catch(){} 捕获。
 
+
+### 业务流程
+1. 实例化 QC类  执行 qq_login() 方法， 会生成一个QQ的登陆URL，访问该URL，进行授权登陆。
+2. 授权登陆成功，会携带 code 跳转至设置的回调地址， 使用 qq_callback() 接收 code值，（code值是获取access_token的凭证），从而获取 access_token
+3. 凭借 access_token 获取 用户的 open_id
+4. 有了 access_token 与 open_id 执行 setKeysArr() 方法，设置接口的公共请求参数。
+5. 执行 get_user_info() 方法获取用户信息，写入数据表 
+
+### 食用说明, Laravel 代码示例
+```$xslt
+    $appid = "102***947"  //应用appid
+    $appkey = "FuMaaMwJT***ew0pP" //应用appkey
+    $callback  = "https://www.***.cn/login/qcallback" //应用授权回调地址
+    $scope = "get_user_info,add_share"  //申请获取的应用权限，多个以英文逗号相隔
+```
+
+```$xslt
+    // qq登录
+    public function qq()
+    {
+        $qc = new QC($appid,$appkey,$callback,$scope);
+       return redirect($qc->qq_login());
+    }
+```
+
+```$xslt
+// 回调操作
+    public function qqCallback(Request $request)
+    {
+        $qc = new QC($appid,$appkey,$callback,$scope);
+
+        if(!$request->get('code')){
+            if(cache('QC_userData')['openid']){
+                $qc->setKeysArr($access_token, $openid);
+                $info = $qc->get_user_info();
+                $data = [
+                    'name'=>$info['nickname'], //昵称
+//                    ''=>$info['gender_type'], // 性别 1男
+//                    ''=>$info['province'], //省份
+//                    ''=>$info['city'], //城市
+//                    ''=>$info['figureurl_2'], //头像
+//                    ''=>$info['year'], // 出生年分
+                ];
+                
+                // TODO 信息获取成功，保存用户数据，实现用户登录
+
+                // 进入登陆成功页
+                return redirect(url('user'));
+            }else{
+                // 授权回调失败，显示404
+                abort(404);
+            }
+        }else{
+            $access_token = $qc->qq_callback();
+        
+            if($access_token){
+                $openid = $qc->get_openid();
+
+                $user = User::where('qq_openid',$openid)->first();
+
+                if($user){
+                    // TODO 信息获取成功，保存用户数据，实现用户登录
+                    
+                    // 缓存数据，access_token有效期30天
+                    cache('QC_userData',['access_token'=>$access_token,'openid'=>$openid], 29*24*3600);
+                    // 进入登陆成功页
+                    return redirect(url('user'));
+                }else{
+                    // 重新访问回调地址
+                    return redirect($callback);
+                }
+
+            }else{
+                return abort(404);
+            }
+        }
+    }
+```
---
+++ src/Oauth.php
@@ -0,0 +1,144 @@
+<?php
+/* PHP SDK
+ * @version 2.0.0
+ * @author connect@qq.com
+ * @copyright © 2013, Tencent Corporation. All rights reserved.
+ */
+
+namespace jonexyz\QQConnect;
+
+class Oauth {
+
+    const VERSION = "2.0";
+    const GET_AUTH_CODE_URL = "https://graph.qq.com/oauth2.0/authorize";
+    const GET_ACCESS_TOKEN_URL = "https://graph.qq.com/oauth2.0/token";
+    const GET_OPENID_URL = "https://graph.qq.com/oauth2.0/me";
+
+    public $urlUtils;
+    protected $recorder;
+
+    protected $appid;
+    protected $appkey;
+    protected $callback;
+    protected $scope;
+
+    /**
+     * Oauth constructor.
+     * @param $appid string 应用appid
+     * @param $appkey string 应用appkey
+     * @param $callback string 应用授权回调地址
+     * @param $scope string  申请获取的应用权限，多个以英文逗号相隔
+     */
+    function __construct($appid,$appkey,$callback,$scope){
+        $this->appid = $appid;
+        $this->appkey = $appkey;
+        $this->callback = $callback;
+        $this->scope = $scope;
+
+        if(empty($this->appid) || empty($this->appkey) || empty($this->callback) || empty($this->scope) ){
+            throw new \Exception('配置信息填写不完整',"20001");
+        }
+
+        $this->urlUtils = new URL();
+    }
+
+    /**
+     *
+     * @param $appid string 应用appid
+     * @param $callback string 应用回调地址
+     * @param $scope string 应用申请的授权内容
+     * @return string
+     */
+    public function qq_login(){
+        //-------生成唯一随机串防CSRF攻击
+        $state = md5(uniqid(rand(), TRUE));
+        $this->recorder->write('state',$state);
+
+        //-------构造请求参数列表
+        $keysArr = array(
+            "response_type" => "code",
+            "client_id" => $this->appid,
+            "redirect_uri" => $this->callback,
+            "state" => $state,
+            "scope" => $this->scope
+        );
+
+        $login_url =  $this->urlUtils->combineURL(self::GET_AUTH_CODE_URL, $keysArr);
+
+        return $login_url;
+    }
+
+    public function qq_callback(){
+
+        //--------验证state防止CSRF攻击
+        /*$state = $this->recorder->read("state");
+        if(!$state || $_GET['state'] != $state){
+            throw new \Exception('The state does not match. You may be a victim of CSRF.',30001);
+        }*/
+
+        //-------请求参数列表
+        $keysArr = array(
+            "grant_type" => "authorization_code",
+            "client_id" => $this->appid,
+            "redirect_uri" => urlencode($this->callback),
+            "client_secret" => $this->appkey,
+            "code" => $_GET['code']
+        );
+
+        //------构造请求access_token的url
+        $token_url = $this->urlUtils->combineURL(self::GET_ACCESS_TOKEN_URL, $keysArr);
+        $response = $this->urlUtils->get_contents($token_url);
+
+        if(strpos($response, "callback") !== false){
+
+            $lpos = strpos($response, "(");
+            $rpos = strrpos($response, ")");
+            $response  = substr($response, $lpos + 1, $rpos - $lpos -1);
+            $msg = json_decode($response);
+
+            if(isset($msg->error)){
+                throw new \Exception($msg->error, $msg->error_description);
+            }
+        }
+
+        $params = array();
+        parse_str($response, $params);
+
+        if(empty($params["access_token"])){
+            throw new \Exception('access_token 获取失败');
+        }
+
+        return $params["access_token"];
+    }
+
+    public function get_openid($access_token){
+        //-------请求参数列表
+        $keysArr = array(
+            "access_token" => $access_token,
+            "unionid"=> 1
+        );
+
+        $graph_url = $this->urlUtils->combineURL(self::GET_OPENID_URL, $keysArr);
+        $response = $this->urlUtils->get_contents($graph_url);
+
+        //--------检测错误是否发生
+        if(strpos($response, "callback") !== false){
+
+            $lpos = strpos($response, "(");
+            $rpos = strrpos($response, ")");
+            $response = substr($response, $lpos + 1, $rpos - $lpos -1);
+        }
+
+        $user = json_decode($response);
+        if(isset($user->error)){
+            throw new \Exception($user->error, $user->error_description);
+        }
+
+        //------记录openid
+        if(empty($user->openid)){
+            throw new \Exception('openid 获取失败');
+        }
+        return $user->openid;
+
+    }
+}
